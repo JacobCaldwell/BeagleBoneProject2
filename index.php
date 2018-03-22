c49816f77f3c5b8d75664f01cfc13f00697210f0
Jacob Caldwell <jscaldwell95@gmail.com>
2018-03-22 00:38:30 -0500

    Final Version

--- index.php
+++ index.php
@@ -1,42 +1,71 @@
 <?php
-    if(isset($_POST['action']) && !empty($_POST['action'])) {
-        $action = $_POST['action'];
-        switch($action) {
-            case 'getServerUptime' : getServerUptime(); break;
-            case 'getIP' : getIP(); break;
-            case 'getSwitch' : getSwitch(); break;
-            case 'getTemp': getTemp(); break;
-            case 'writeTemp': writeTemp(); break;
+
+    if (!empty($argv[1])) {
+        switch ($argv[1]) {
+            case "get_temp_data":
+                post_temp(date('Y-m-d H:i'));
+                break;
         }
     }
 
-    function getServerUptime()
+    if(isset($_GET['action']) && !empty($_GET['action'])) {
+        $action = $_GET['action'];
+        $param = $_GET['param'];
+        switch($action) {
+            case 'get_serv_up' : get_serv_up(); break;
+            case 'get_ip' : get_ip(); break;
+            case 'get_switch' : get_switch(); break;
+            case 'get_temp': get_temp(); break;
+            case 'update_temp': update_temp(); break;
+
+        }
+    }
+
+    function get_serv_up()
     {
         echo @file_get_contents('/proc/uptime');
     }
 
-    function getIP(){
+    function get_ip(){
         echo $_SERVER['SERVER_ADDR'];
     }
 
-    function getSwitch(){
+    function get_switch(){
         echo @file_get_contents('/sys/class/gpio/gpio68/value');
         echo @file_get_contents('/sys/class/gpio/gpio67/value');
     }
 
-    function getTemp()
+    function get_temp()
     {
         $temp = @file_get_contents('/sys/bus/iio/devices/iio:device0/in_voltage1_raw');
         $temp = ((int)$temp*(1800/4096)-500)/10;
         echo round((9/5)*$temp+32, 2);
+
     }
 
-    function writeTemp(){
-        $file = fopen("temp.json","w");
-        fwrite($file,"{asdfk:asdfasd}");
-        fclose($file);
+    function return_temp(){
+        $temp = @file_get_contents('/sys/bus/iio/devices/iio:device0/in_voltage1_raw');
+        $temp = ((int)$temp*(1800/4096)-500)/10;
+        return round((9/5)*$temp+32, 2);
+    }
 
+    function post_temp($date){
+        $temp = return_temp();
+        $file = file_get_contents('/var/www/html/data.json');
+        $array = json_decode($file, true);
+        $extra = ['temp'=> $temp,'date'=>$date];
+        $array['data'][] = $extra;
+        if (count($array['data']) > 24){
+            array_shift($array['data']);
+        }
+        $encoded = json_encode($array);
+        file_put_contents('/var/www/html/data.json',$encoded);
+    }
+
+    function update_temp(){
+        echo file_get_contents('/var/www/html/data.json');
     }
 
 
 ?>
+
--- javascript.js
+++ javascript.js
@@ -3,25 +3,29 @@ var current_switch_state = '';
 $(document).ready(function(){
 	var current_date = function(){
     		var date = new Date();
+    		if (date.getMinutes() == 0 && date.getSeconds() == 5){
+    		    update_temp();
+            }
     		date = date.toString().split("GMT")[0]
 
      		 $("#current-time").text(date);
  	 }
-    getIP();
+    get_ip();
 	current_date();
-	getUp();
-	getTemp();
+	get_serv_up();
+	get_temp();
+	update_temp();
 	setInterval(current_date, 1000);
-	setInterval(getUp,1000);
-	setInterval(getSwitch,100);
-	setInterval(getTemp,5000);
+	setInterval(get_serv_up,1000);
+	setInterval(get_switch,100);
+	setInterval(get_temp,5000);
 });
 
-function getUp(){
+function get_serv_up(){
 	var uptime ='';
     $.ajax({ url: 'index.php',
-        data: {action: 'getServerUptime'},
-        type: 'post',
+        data: {action: 'get_serv_up'},
+        type: 'get',
         success: function(data) {
 			uptime = parseInt(data.split(" ")[0]);
 			$('#server-uptime').text(seconds_to_dhms(uptime));
@@ -29,46 +33,52 @@ function getUp(){
     });
 }
 
-function getIP(){
-    var uptime ='';
+function get_ip(){
     $.ajax({ url: 'index.php',
-        data: {action: 'getIP'},
-        type: 'post',
+        data: {action: 'get_ip'},
+        type: 'get',
         success: function(data) {
             $('#server-ip').text(data);
         }
     });
 }
 
-function writeTemp(){
-    var uptime ='';
+function get_switch(){
     $.ajax({ url: 'index.php',
-        data: {action: 'writeTemp'},
-        type: 'post',
-        success: function(data) {
-            // $('#server-ip').text(data);
-        }
-    });
-}
-
-function getSwitch(){
-    var uptime ='';
-    $.ajax({ url: 'index.php',
-        data: {action: 'getSwitch'},
-        type: 'post',
+        data: {action: 'get_switch'},
+        type: 'get',
         success: function(data) {
             change_switch(data.replace(/[\n\r]+/g, ''));
         }
     });
 }
 
-function getTemp(){
-    var uptime ='';
+function get_temp(){
     $.ajax({ url: 'index.php',
-        data: {action: 'getTemp'},
-        type: 'post',
+        data: {action: 'get_temp'},
+        type: 'get',
         success: function(data) {
             $("#current-temp").text(data);
+        }
+    });
+}
+
+function update_temp(){
+    $.ajax({ url: 'index.php',
+        data: {action: 'update_temp'},
+        type: 'get',
+        success: function(data) {
+            obj = JSON.parse(data);
+
+            var temp_list = [];
+            $("#temp_history").empty();
+            obj.data.forEach(function(element){
+                temp_list.push(parseFloat(element.temp));
+                $(''+'<li class="list-group-item d-flex justify-content-between align-items-center">'+element.date+'<span class="badge badge-primary badge-pill">'+element.temp+'ºF</span> </li>').prependTo("#temp_history");
+            });
+            temp_list.sort();
+            $("#low-temp").text(temp_list[0]);
+            $("#high-temp").text(temp_list[temp_list.length-1]);
 
         }
     });
